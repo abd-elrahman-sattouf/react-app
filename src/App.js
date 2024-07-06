2f099e9dec9e2340dd89506b2c19f97cad10cd9f
abd-elrahman-sattouf <ans015lb@aou.edu.lb>
2024-07-06 23:33:31 +0300

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
