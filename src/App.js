a001156ca0f55a14ff6ce1d9b2941aeece7f2d5b
abd-elrahman-sattouf <ans015lb@aou.edu.lb>
2024-07-06 23:11:47 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import Counter from './hooks/useStat/counter';
-import { Counter2, Counter3 }from'./hooks/useStat/counter';
-import Toggle from './hooks/useStat/toggle';
-import { Toggle2 } from './hooks/useStat/toggle';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>react</h1>
-      {/* <Counter />
-      <Counter2 />
-      <Counter3 /> */}
-      <Toggle />
-      <Toggle2 />
+      
     </div>
   );
 }
